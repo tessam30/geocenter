f38ffb571c1e346a9caddf52da3128bff6457e87
flaneuse <laura.d.hughes@gmail.com>
2016-09-23 10:26:32 -0400

    modernize
    rename function, + folder option, rely on read_shp

--- R/importShp.R
+++ R/importShp.R
@@ -1,4 +1,4 @@
-#' Import ESRI shapefiles into R
+#' Import ESRI shapefiles into R data frame
 #'
 #' Import shapefile and convert into lat/lon coordinates to plot in R or Tableau
 #'
@@ -13,8 +13,9 @@
 #' @import maptools
 #' @import dplyr
 #'
-#' @param workingDir string containing the name of the folder containing the shapefile
-#' @param layerName string containing the name of the shapefile, e.g. 'district_boundary' for 'district_boundary.shp'
+#' @param baseDir string containing the name of the base directotry containing the shapefile
+#' @param folderName (optional) string containing the name of the folder within baseDir containing the shapefile. Not required if baseDir contains the full name of the folder containing the shapefile
+#' @param layerName string containing the name of the shapefile, e.g. 'district_boundary' for 'district_boundary.shp'. Should not include '.shp'
 #' @param exportData Boolean for whether to save the data as a .csv after importing
 #' @param fileName string containing the name of the .csv to be exported; by default, will be the same as the inputted .shp file
 #' @param labelVar string containing the variable name containing the names of the polygons (for instance, the names of the provinces, districts, etc.)
@@ -24,15 +25,17 @@
 #' @export
 #' @examples
 #'
-#'
-#'
 
-importShp = function(workingDir = getwd(),
-                     layerName,
-                     exportData = TRUE,
-                     fileName = layerName,
-                     labelVar = NA,
-                     reproject = TRUE, projection = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") {
+
+# -- Function to import shapefiles and convert to a ggplot-able object --
+shp2df = function(baseDir = getwd(),
+                  folderName = NULL,
+                  layerName,
+                  exportData = TRUE,
+                  fileName = layerName,
+                  getCentroids = TRUE,
+                  labelVar = NA,
+                  reproject = TRUE, projection = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") {
 
   # Check that the layerName doesn't contain any extensions
   # Check that layerName exists within the wd
@@ -40,11 +43,8 @@ importShp = function(workingDir = getwd(),
   # Log the current working directory, to change back at the end.
   currentDir = getwd()
 
-  # Change directory to the file folder containing the shape file
-  setwd(workingDir)
-
-  # the dsn argument of '.' says to look for the layer in the current directory.
-  rawShp = rgdal::readOGR(dsn = ".", layer = layerName)
+  # Read in the raw shapefile
+  rawShp = read_shp(baseDir = baseDir, folderName = folderName, layerName = layerName)
 
   if (reproject == TRUE) {
     # reproject the data
@@ -61,26 +61,38 @@ importShp = function(workingDir = getwd(),
   # Merge the polygon lat/lon points with the original data
   df = dplyr::left_join(poly_points, projectedShp@data, by = "id")
 
+  if (getCentroids == TRUE){
+    # Pull out the centroids and the associated names.
+    centroids = data.frame(coordinates(projectedShp)) %>% rename(long = X1, lat = X2)
 
-  # Pull out the centroids and the associated names.
-  centroids = data.frame(coordinates(projectedShp)) %>% rename(long = X1, lat = X2)
-
-  if (!is.na(labelVar)) {
-    if (labelVar %in% colnames(projectedShp@data)) {
-      # Merge the names with the centroids
-      centroids = cbind(centroids, projectedShp@data[labelVar]) %>% rename_(label = labelVar)  # rename the column
-    } else {
-      warning("label variable for the centroids is not in the raw shapefile")
+    if (!is.na(labelVar)) {
+      if (labelVar %in% colnames(projectedShp@data)) {
+        # Merge the names with the centroids
+        centroids = cbind(centroids, projectedShp@data[labelVar]) %>% rename_(label = labelVar)  # rename the column
+      } else {
+        warning("label variable for the centroids is not in the raw shapefile")
+      }
     }
+
+    # if the 'exportData' option is selected, save the lat/lon coordinates as a .csv
+    if (exportData == TRUE) {
+      write.csv(df, paste0(baseDir, "/", fileName, ".csv"))
+      write.csv(centroids, paste0(baseDir, "/", fileName, "_centroids.csv"))
+    }
+
+
+    # Return the dataframe containing the coordinates and the centroids
+    return(list(df = df, centroids = centroids))
+  } else {
+    # if the 'exportData' option is selected, save the lat/lon coordinates as a .csv
+    if (exportData == TRUE) {
+      write.csv(df, paste0(baseDir, "/", fileName, ".csv"))
+    }
+
+    # Reset the working directory
+    setwd(currentDir)
+
+    # Return the dataframe containing the coordinates and the centroids
+    return(df)
   }
-
-  # if the 'exportData' option is selected, save the lat/lon coordinates as a .csv
-  if (exportData == TRUE) {
-    write.csv(df, paste0(workingDir, "/", fileName, ".csv"))
-    write.csv(centroids, paste0(workingDir, "/", fileName, "_centroids.csv"))
-  }
-
-
-  # Return the dataframe containing the coordinates and the centroids
-  return(list(df = df, centroids = centroids))
 }
