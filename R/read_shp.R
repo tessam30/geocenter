99b791d246b608a98fa5a6f7f5dd13a4c5bff7af
flaneuse <laura.d.hughes@gmail.com>
2016-09-23 10:14:18 -0400

    sep WD to base + folder

--- R/read_shp.R
+++ R/read_shp.R
@@ -1,6 +1,7 @@
 #' Import ESRI shapefiles into R
 #'
-#' Import shapefile into an R spatial object
+#' Import shapefile into an R spatial polygons object. Useful for plotting within the leaflet package,
+#' or for downstream use to convert into a series of lat/lon coordinates.
 #'
 #'
 #' @author Laura Hughes, laura.d.hughes@gmail.com
@@ -8,13 +9,14 @@
 #' @import rgdal
 #'
 #' @param workingDir string containing the name of the folder containing the shapefile
-#' @param layerName string containing the name of the shapefile, e.g. 'district_boundary' for 'district_boundary.shp'
+#' @param layerName string containing the name of the shapefile, e.g. 'district_boundary' for 'district_boundary.shp'. Should not include '.shp'
 #'
 #' @export
 #' @examples
 
 # -- Function to import shapefiles --
-read_shp = function(workingDir = getwd(),
+read_shp = function(baseDir = getwd(),
+                    folderName = NULL,
                     layerName) {
   # Check that the layerName doesn't contain any extensions
   # Check that layerName exists within the wd
@@ -23,8 +25,8 @@ read_shp = function(workingDir = getwd(),
   currentDir = getwd()
 
   # Change directory to the file folder containing the shape file
-  setwd(workingDir)
+  setwd(paste0(baseDir, folderName))
 
   # the dsn argument of '.' says to look for the layer in the current directory.
-  rawShp = rgdal::readOGR(dsn = ".", layer = layerName)
+  rawShp = readOGR(dsn = ".", layer = layerName)
 }
