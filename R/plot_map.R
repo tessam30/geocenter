d184d302bef5be929882422eb1002aae418d53e0
flaneuse <laura.d.hughes@gmail.com>
2016-11-15 08:22:42 -0500

    save map

--- R/plot_map.R
+++ R/plot_map.R
@@ -14,8 +14,12 @@
 plot_map = function(df,
                     fill_var,
                     stroke_size = 0.15,
-                    stroke_colour = 'white') {
-  ggplot(df, aes_string(x = 'long', y = 'lat',
+                    stroke_colour = 'white',
+                    file_name = NA,
+                    plot_width = 5,
+                    plot_height = 5
+                    ) {
+  p = ggplot(df, aes_string(x = 'long', y = 'lat',
                   group = 'group', order = 'order',
                   fill = fill_var)) +
     geom_polygon() +
@@ -24,4 +28,10 @@ plot_map = function(df,
     coord_equal() +
      theme_void() +
     theme(legend.position = 'none')
+
+  if(!is.na(file_name)) {
+    llamar::save_plot(file_name, width = plot_width, height = plot_height)
+  }
+
+  return(p)
 }
