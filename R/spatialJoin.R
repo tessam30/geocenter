f6f983eac08e0c10bc512103ca54890220f35d16
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-31 10:44:08 -0400

    update documentation

--- R/spatialJoin.R
+++ R/spatialJoin.R
@@ -3,22 +3,26 @@
 #' @description Imports a shp file and a data frame loaded into R, and does a spatial
 #' join between the points in the data frame and polygons in the shapefile
 #'
-#' @import sf sp dplyr purr
+#' @import sf sp dplyr purrr
 #' @export
+#'
+#' @param shp either a shapefile file location, or a spatial data frame imported using sf::st_read
+#' @param df data frame containing latitudes and longtitudes of the points to spatially join to `shp`. Assumes the data are organized in a data frame containing columns lat_var and lon_var
+#' @param lat_var string specifying column in `df` that contains the latitude coordinates.
+#' @param lon_var string specifying column in `df` that contains the longitude coordinates.
+#' @param proj_crs PROJ4 string specifying the desired projection to use in spatial join. For help choosing projection see http://projectionwizard.org/
+#' @param df_crs PROJ4 string specifying the coordinate reference system for `df`. Assumed to be WGS84 if not specified.
+#' @param binary_var string specifiying the name of a new binary variable created in the output for whether the point is within any polygon in `shp` or not
+#' @param merge2shp TRUE/FALSE if `df` should be merged with the contents of `shp`
+#' @param plot_results TRUE/FALSE if a leaflet map of the join should be plotted
+#'
 #'
 #' @examples
 #' n = 2000
 #' df = data.frame(lon = sample(round(32.67152*100):round(35.91505*100), n, replace = TRUE)/100, lat = sample(round(-17.12721*100):round(-13.34228*100), n, replace = TRUE)/100)
 
 
-library(sf)
-library(ggplot2)
-library(leaflet)
-library(dplyr)
-library(purrr)
-library(llamar)
-
-spatial_join = function (shp_file,
+spatial_join = function (shp,
                          df,
                          proj_crs,
                          lat_var = 'lat',
@@ -27,9 +31,22 @@ spatial_join = function (shp_file,
                          binary_var = 'inPolygon',
                          merge2shp = TRUE,
                          plot_results = TRUE) {
+
   # Part 1. -- Shapefile import --
-  # Read in the shapefile
-  shp = st_read(shp_file)
+  # Check if shp is an imported sf object
+  if(grepl('\\.shp', shp)) {
+    # Read in the shapefile
+    shp = st_read(shp)
+  } else if('geometry' %in% colnames(shp)) {
+    if (!is.list(shp$geometry)){
+      stop('unknown shapefile input.  Please input either the name of a shapefile,
+         or an sf-imported spatial data frame using sf::st_read')
+    }
+  } else {
+    stop('unknown shapefile input.  Please input either the name of a shapefile,
+         or an sf-imported spatial data frame using sf::st_read')
+  }
+
 
 
   shp = shp %>%
@@ -40,6 +57,10 @@ spatial_join = function (shp_file,
 
 
   # Part 2. -- Define data frame as a spatial object --
+  if(!lat_var %in% colnames(df) | !lon_var %in% colnames(df)) {
+    error('Latitutde and/or longitude variables are not found in `df`. Do you need to change `lat_var` or `lon_var`?')
+  }
+
   # using sp package, define the variables that contain lat/long to convert to spatial object
   coordinates(df) = as.formula(paste0('~', lon_var, '+', lat_var))
   # convert from sp spatial obj to sf spatial obj
